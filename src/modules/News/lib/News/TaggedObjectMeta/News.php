0d701befad04b34a310ddb0aa7db10aafabad21d
Craig Heydenburg <craigh@mac.com>
2011-07-07 09:58:06 -0400

    add additional checks in Tag plugin for status and pending/expiry

--- src/modules/News/lib/News/TaggedObjectMeta/News.php
+++ src/modules/News/lib/News/TaggedObjectMeta/News.php
@@ -16,7 +16,11 @@ class News_TaggedObjectMeta_News extends Tag_AbstractTaggedObjectMeta
         parent::__construct($objectId, $areaId, $module, $objectUrl);
 
         $newsItem = ModUtil::apiFunc('News', 'user', 'get', array('sid' => $this->getObjectId()));
-        if ($newsItem) {
+        // the api takes care of the permissions check. we must check for pending/expiration & status
+        $expired = (isset($newsItem['to']) && (strtotime($newsItem['to']) < strtotime("now")));
+        $pending = (strtotime($newsItem['from']) > strtotime("now"));
+        $statuspublished = ($newsItem['published_status'] == News_Api_User::STATUS_PUBLISHED);
+        if ($newsItem && $statuspublished && !$pending && !$expired) {
             $this->setObjectAuthor($newsItem['contributor']);
             $this->setObjectDate($newsItem['from']);
             $this->setObjectTitle($newsItem['title']);
