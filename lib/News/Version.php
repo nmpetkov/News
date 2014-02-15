1011590e832d0471e69ac5973a5d68561e3c468e
Craig Heydenburg <craigh@mac.com>
2014-02-15 09:15:28 -0500

    raise core_max

--- lib/News/Version.php
+++ lib/News/Version.php
@@ -25,7 +25,7 @@ class News_Version extends Zikula_AbstractVersion
         //! this defines the module's url
         $meta['url'] = $this->__('news');
         $meta['core_min'] = '1.3.0'; // requires minimum 1.3.0 or later
-        $meta['core_max'] = '1.3.99'; // Fixed to 1.3.x range
+        $meta['core_max'] = '1.4.99';
         $meta['capabilities'] = array(HookUtil::SUBSCRIBER_CAPABLE => array('enabled' => true));
         $meta['securityschema'] = array('News::' => 'Contributor ID::Article ID',
                 'News:pictureupload:' => '::',
